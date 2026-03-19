bbfa1c0e1d49ada9f4193bac7353e41c3e141f21
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2026-03-19 16:08:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,22 @@ st.title("🎬 Movie Recommender System")
 def load_data():
     movies = pd.read_csv("movies.csv", nrows=1000)
     ratings = pd.read_csv("ratings.csv", nrows=5000)
-    return movies, ratings
+    
+    movie_stats = ratings.groupby('movieId')['rating'].agg(['mean','count']).reset_index()
+    movie_stats.rename(columns={'mean':'AvgRating', 'count':'TotalRatings'}, inplace=True)
+    
+    movies = movies.merge(movie_stats, on='movieId', how='left')
+    movies['AvgRating'] = movies['AvgRating'].fillna(0)
+    
+    return movies
 
-movies, ratings = load_data()
+movies = load_data()
 
-st.success("Data loaded successfully ✅")
-st.write(movies.head())
+st.sidebar.title("Options")
+top_n = st.sidebar.slider("Number of movies", 3, 10, 5)
+
+if st.button("Recommend Top Movies"):
+    top_movies = movies.sort_values(by='AvgRating', ascending=False).head(top_n)
+    
+    for _, row in top_movies.iterrows():
+        st.write(f"🎬 {row['title']} ({round(row['AvgRating'],2)})")
