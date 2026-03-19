7ebb83215ca677a6f3c1825780abea0e2c638cba
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2026-03-19 16:08:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.title("It works ✅")
+import pandas as pd
+
+st.title("🎬 Movie Recommender System")
+
+@st.cache_data
+def load_data():
+    movies = pd.read_csv("movies.csv", nrows=1000)
+    ratings = pd.read_csv("ratings.csv", nrows=5000)
+    return movies, ratings
+
+movies, ratings = load_data()
+
+st.success("Data loaded successfully ✅")
+st.write(movies.head())
