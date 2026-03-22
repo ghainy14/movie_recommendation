29afcf2a668fdfc03069bd0f6788b8a17e92094e
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2026-03-22 02:40:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ h1, h2, h3 {
 </style>
 """, unsafe_allow_html=True)
 st.title("🎬 Movie Recommender System")
-st.markdown("<h1 style='text-align: center;'>🎬 Movie Recommender System</h1>", unsafe_allow_html=True)
 @st.cache_data
 def load_data():
     movies = pd.read_csv("movies.csv", nrows=1000)
