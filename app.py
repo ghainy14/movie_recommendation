0e7517f74966ca5b32a22dcc51c07b0008ed6d56
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2026-03-22 02:49:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,8 @@ if st.button("Get Similar Movies"):
     recs = subset_movies[subset_movies['movieId'].isin(rec_ids)]
     
 st.subheader("🎥 Recommended Movies")
-for _, row in recs.iterrows():
-    st.markdown(f"""
+    for _, row in recs.iterrows():
+        st.markdown(f"""
         <div class="movie-card">
             <h4>{row['title']}</h4>
             <p>⭐ Rating: {round(row['AvgRating'],2)}</p>
