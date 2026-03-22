bd30c24ea59dfdaddbd121d8052f430629fa7dc7
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2026-03-22 02:56:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,12 @@ if st.button("Get Similar Movies"):
     rec_ids = sim_scores.index
     
     recs = subset_movies[subset_movies['movieId'].isin(rec_ids)]
-st.subheader("Recommended Movies")
-for _, row in recs.iterrows():
-      st.write(row['title'])
+
+    st.subheader("🎥 Recommended Movies")
+
+    for _, row in recs.iterrows():
+        st.markdown(f"""
+            <div class="movie-card">
+                <h4>{row['title']}</h4>
+            </div>
+        """, unsafe_allow_html=True)
