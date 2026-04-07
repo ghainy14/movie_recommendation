7ea0409a9a511266907ebf3ca7b56422497b8818
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2026-04-07 13:19:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ theme = st.sidebar.radio("Choose Theme", ["Dark", "Light"])
 
 if theme == "Dark":
     bg_color = "#0E1117"
-    card_color = "#26273"
+    card_color = "#262730"
     text_color = "red"
 else:
     bg_color = "#F5F5F5"
